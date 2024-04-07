8f550f18f8affa16e9cbd63ebf8c6f2941f2e3db
beckyparkes <rebekahmparkes@outlook.com>
2024-04-06 23:27:59 -0500

    not functional, map thru questions and answers

--- js-fiddling/src/components/Math.js
+++ js-fiddling/src/components/Math.js
@@ -2,42 +2,41 @@ import { useState } from "react";
 const Math = () => {
     const [ans, setAns] = useState(undefined)
     const [result, setResult] = useState(undefined)
-    
+
     const questionsAndAnswers = [
         ["1 + 1", 2],
         ["1 + 2", 3],
         ["2 + 2", 4]
     ];
 
-    const selectedIndex = qaArray => {
-        return Math.floor(Math.random() * qaArray.length);
-    }
-
-    const question = questionsAndAnswers[0][0];
-    const answer = questionsAndAnswers[0][1];
-
     const handleChange = e => {
         setAns(e.target.value)
     }
 
-    const handleSubmit = e => {
-        e.preventDefault()
-        setResult(ans == 2)
+    const handleSubmit = (a) => {
+        setResult(ans == a)
     }
 
-    return <>
-        <div>{question}</div>
-        <form onSubmit={handleSubmit}>
-            <input type="number" onChange={handleChange} />
-            <button>Submit</button>
-        </form>
-        {result &&
-            <div>That's correct</div>
-        }
-        {result == false &&
-            <div>That's incorrect</div>
-        }
-    </>
+    return (<div>
+        {questionsAndAnswers.map((qAndA) => {
+            const q = qAndA[0]
+            const a = qAndA[1]
+            return <>
+                <div>{q}</div>
+                <form onSubmit={handleSubmit(a)}>
+                    <input type="number" onChange={handleChange} />
+                    <button>Submit</button>
+                </form>
+                {result &&
+                    <div>That's correct</div>
+                }
+                {result == false &&
+                    <div>That's incorrect</div>
+                }
+            </>
+        })}
+    </div>)
+
 }
 
 export default Math
