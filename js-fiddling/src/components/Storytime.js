7df9d286bd1799cadb4b82647e1c743b0b7673a3
beckyparkes <rebekahmparkes@outlook.com>
2024-04-08 22:25:24 -0500

    functional first select element

--- js-fiddling/src/components/Storytime.js
+++ js-fiddling/src/components/Storytime.js
@@ -1,5 +1,12 @@
 const Storytime = () => {
-    return <></>
+    return <>
+        <label for="foods">Choose a food group:</label>
+        <select aria-placeholder="" name="foods">
+            <option value="unselected" disabled selected hidden></option>
+            <option value="fruit">Fruit</option>
+            <option value="veg">Veggies</option>
+        </select>
+    </>
 }
 
 export default Storytime
