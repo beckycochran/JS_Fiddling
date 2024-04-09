6ee75739b97b54759deecc5e9d622bb5ae1f41cf
beckyparkes <rebekahmparkes@outlook.com>
2024-04-08 23:30:11 -0500

    save progress

--- js-fiddling/src/components/Storytime.js
+++ js-fiddling/src/components/Storytime.js
@@ -6,16 +6,22 @@ const Storytime = () => {
 
     const handleSubmit = e => {
         e.preventDefault()
-        console.log(e)
+        console.log(e.target.innerText)
+        // if any selects are empty, return error/alert and highlight empty element. else, publish with timestamp using date-fns
         setIsPublished(true)
     }
 
     return <>
         <form onSubmit={handleSubmit}>
             <section>
-                There once was a prince named <StorytimeOption name={"mainChar"} options={["Paul", "Peter", "Patrick"]}/>,
-                who loved to <StorytimeOption name={"mainCharMov"} options={["prance", "somersault", "stumble"]}/>
-                into the woods and <StorytimeOption name={"mainCharJob"} options={["whittle toy trains", "pickle peppers", "yodel"]}/>.
+                There once was a prince named <StorytimeOption name={"mainChar"} options={["Paul", "Peter", "Patrick"]} />,
+                who loved to <StorytimeOption name={"mainCharMov"} options={["prance", "somersault", "stumble"]} /> into the woods and <StorytimeOption name={"mainCharJob"} options={["whittle toy trains", "pickle peppers", "yodel"]} />.
+            </section>
+            <section>
+                The prince encountered a fair maiden named <StorytimeOption name={"princess"} options={["Delilah", "Diane", "Deanna"]} />,
+                whose hair was <StorytimeOption name={"princessHair"} options={["brown as burgundy", "soft as a tortoses' shell", "long as a gnat's wings"]} />.
+                They took to <StorytimeOption name={"dateActivity"} options={["making pumpernickel sandwiches", "watching hulu", "rock climbing in Yosemite"]} /> together,
+                and fell wildly in love.
             </section>
             <button type="submit">Publish!</button>
         </form>
--- js-fiddling/src/components/StorytimeOption.js
+++ js-fiddling/src/components/StorytimeOption.js
@@ -1,8 +1,13 @@
 const StorytimeOption = ({ name,  options }) => {
+    const handleSelect = e => {
+        console.log(e.target)
+        console.log("select val", e.target.value)
+    }
+
     return <>
-        <select defaultValue="" name={name}>
+        <select defaultValue="" onChange={handleSelect} name={name}>
             <option value="" disabled hidden></option>
-            {options.map((e, i) => <option key={i} value={e}>{e}</option>)}
+            {options.map((e, i) => <option key={i} value={e} >{e}</option>)}
         </select>
     </>
 }
